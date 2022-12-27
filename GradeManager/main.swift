1c63b3b78afd54c94f66be5b06be3c93c5f2a882
kakao <kakao@summer-love-pc.local>
2022-12-27 17:11:56 +0900

    feat: addStudent, deleteStudent 구현

--- GradeManager/main.swift
+++ GradeManager/main.swift
@@ -35,11 +35,18 @@ struct InputManager {
             return .failure(.wrong)
         }
     }
+    
+    func getStudentName() -> Result<String, InputError> {
+        guard let name = readLine() else {
+            return .failure(.null)
+        }
+        return .success(name)
+    }
 }
 
 protocol StudentManager {
-    func add()
-    func delete()
+    mutating func add(name: String)
+    mutating func delete(name: String)
 }
 
 struct Student: Hashable {
@@ -48,30 +55,78 @@ struct Student: Hashable {
     static func == (lhs: Student, rhs: Student) -> Bool {
         return lhs.name == rhs.name
     }
+    
+    static func == (lhs: Student, rhs: String) -> Bool {
+        return lhs.name == rhs
+    }
 }
 
 struct DefaultStudentManager: StudentManager {
+    enum InfoMessage: String {
+        case addSuccess = " 학생을 추가했습니다."
+        case addFailure = "입력이 잘못되었습니다. 다시 확인해주세요."
+        case deleteSuccess = "학생을 삭제하였습니다."
+        case deleteFailure = " 학생을 찾지 못했습니다."
+        
+        func printing(name: String) {
+            switch self {
+            case .addSuccess, .deleteFailure:
+                print(name, terminator: "")
+                print(self.rawValue)
+            default:
+                print(self.rawValue)
+            }
+        }
+    }
+    
     private var list = Set<Student>()
     
     func isDuplicate(name: String) -> Bool {
-        
+        for student in list {
+            if student == name {
+                return true
+            }
+        }
+        return false
     }
     
-    func add() {
-        
+    func deleteTarget(name: String) -> Student? {
+        for student in list {
+            if student == name {
+                return student
+            }
+        }
+        return nil
     }
     
-    func delete() {
-        
+    mutating func add(name: String) {
+        if isDuplicate(name: name) {
+            InfoMessage.addFailure.printing(name: name)
+            return
+        }
+        list.insert(Student(name: name))
+        InfoMessage.addSuccess.printing(name: name)
+    }
+    
+    mutating func delete(name: String) {
+        guard let deleteTargetStudent = deleteTarget(name: name) else {
+            InfoMessage.deleteFailure.printing(name: name)
+            return
+        }
+        list.remove(deleteTargetStudent)
+        InfoMessage.deleteSuccess.printing(name: name)
     }
 }
 
 struct GradeManager {
     let inputManager = InputManager()
-    private let studentManager: StudentManager? = DefaultStudentManager()
+    private var studentManager: StudentManager = DefaultStudentManager()
     
     enum InfoMessage: String {
         case guide = "원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료"
+        case add = "추가할 학생의 이름을 입력해주세요"
+        case delete = "삭제할 학생의 이름을 입력해주세요"
+        case exit = "프로그램을 종료합니다..."
         
         func printing() {
             print(self.rawValue)
@@ -89,27 +144,53 @@ struct GradeManager {
         }
     }
     
-    func operate(menu: String) {
+    mutating func addStudent() {
+        InfoMessage.add.printing()
+        let studentNameResult = inputManager.getStudentName()
+        switch studentNameResult {
+        case .success(let studentName):
+            studentManager.add(name: studentName)
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+    
+    mutating func deleteStudent() {
+        InfoMessage.delete.printing()
+        let studentNameResult = inputManager.getStudentName()
+        switch studentNameResult {
+        case .success(let studentName):
+            studentManager.delete(name: studentName)
+        case . failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+    
+    mutating func operate(menu: String) {
         switch menu {
         case "1":
-            
+            addStudent()
         case "2":
-            
+            deleteStudent()
         default:
             break
         }
     }
     
-    func run() {
+    mutating func run() {
         while true {
             InfoMessage.guide.printing()
             guard let menu = menuChoice() else {
                 continue
             }
             if menu == "X" {
+                InfoMessage.exit.printing()
                 break
             }
             operate(menu: menu)
         }
     }
 }
+
+var gradeManager = GradeManager()
+gradeManager.run()
