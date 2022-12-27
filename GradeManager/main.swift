9ae2c36186d6690ad2f9ec012e764f60db61de16
volgaHwang <wltkak2@gmail.com>
2022-12-27 14:18:34 +0900

    feat: InputError 구현

--- GradeManager/main.swift
+++ GradeManager/main.swift
@@ -5,3 +5,19 @@
 // 
 
 import Foundation
+
+enum InputError: Error {
+    case null
+    case wrong
+}
+
+extension InputError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .null:
+            return "입력에 문제가 있습니다. 다시 입력해주세요."
+        case .wrong:
+            return "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요."
+        }
+    }
+}
