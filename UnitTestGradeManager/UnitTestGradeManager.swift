4c5ea05d5f86fc14eaef0a79bb49e911608120e1
kakao <kakao@summer-love-pc.local>
2022-12-27 11:57:59 +0900

    test: input에 대한 test case 추가

--- UnitTestGradeManager/UnitTestGradeManager.swift
+++ UnitTestGradeManager/UnitTestGradeManager.swift
@@ -8,6 +8,78 @@
 import XCTest
 
 class UnitTestGradeManager: XCTestCase {
+    
+    let sut = GradeManager()
+    
+    func test_input_menu_1() {
+        let result = sut.menuChoice()
+        switch result {
+        case .success(let data):
+            XCTAssertEqual(data, "1")
+        case .failure:
+            XCTFail()
+        }
+    }
+    
+    func test_input_menu_2() {
+        let result = sut.menuChoice()
+        switch result {
+        case .success(let data):
+            XCTAssertEqual(data, "2")
+        case .failure:
+            XCTFail()
+        }
+    }
+    
+    func test_input_menu_3() {
+        let result = sut.menuChoice()
+        switch result {
+        case .success(let data):
+            XCTAssertEqual(data, "3")
+        case .failure:
+            XCTFail()
+        }
+    }
+    
+    func test_input_menu_4() {
+        let result = sut.menuChoice()
+        switch result {
+        case .success(let data):
+            XCTAssertEqual(data, "4")
+        case .failure:
+            XCTFail()
+        }
+    }
+    
+    func test_input_menu_5() {
+        let result = sut.menuChoice()
+        switch result {
+        case .success(let data):
+            XCTAssertEqual(data, "5")
+        case .failure:
+            XCTFail()
+        }
+    }
+    
+    func test_input_menu_X() {
+        let result = sut.menuChoice()
+        switch result {
+        case .success(let data):
+            XCTAssertEqual(data, "X")
+        case .failure:
+            XCTFail()
+        }
+    }
+    
+    func test_input_menu_error() {
+        let result = sut.menuChoice()
+        switch result {
+        case .success:
+            XCTFail()
+        case .failure:
+            XCTAssert(true)
+        }
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
