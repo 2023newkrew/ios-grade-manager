4804adad4d8b17eea7a2bbf5fdbccdc0555745eb
volgaHwang <wltkak2@gmail.com>
2022-12-29 17:24:39 +0900

    feat: CouresManager를 추가하여 Student가 소유하게 했고, 이를 통해 학생 성적을 관리하는 기능을 추가하였습니다.
    - Student의 관리를 위해서 Class로 변경 했습니다.
        - StudentManager에서 Set<Student>를 순회하는 경우에 Struct를 사용 할 경우 값이 복사가 되어 따로 주소값을 통한 Indicating이 불가능한 상황이고 따라서 원본에 필요한 메소드를 출력하지 못하여 Class로 전환했습니다.
    - CourseManager 구조체에선 dictionary 구조로 [과목명: 성적Enumerate] 구조를 사용하여 과목별 성적을 관리합니다.
        - 관리되는 딕셔너리의 list를 통해 CourseManager는 상기 목표였던 성적 추가 및 변경 그리고 삭제 기능까지 추가하였습니다.
    - 추가적으로 리팩토링 한 부분이 있는데, isDuplicate는 의미가 모호하여 isExist와 같은 네이밍으로 변경을 진행한 부분이 있습니다.

--- GradeManager/main.swift
+++ GradeManager/main.swift
@@ -1,13 +1,14 @@
 //
 //  GradeManager - main.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom. All rights reserved.
-// 
+//
 
 import Foundation
 
 enum InputError: Error {
     case invalidUserInput
+    case invalidStudentGradeInfoInput
 }
 
 extension InputError: LocalizedError {
@@ -15,6 +16,8 @@ extension InputError: LocalizedError {
         switch self {
         case .invalidUserInput:// 바꿀 예정 invalidUserInput
             return "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요."
+        case .invalidStudentGradeInfoInput:
+            return "입력이 잘못되었습니다. 다시 확인해주세요."
         }
     }
 }
@@ -29,6 +32,41 @@ enum Menu: String {
 }
 
 struct InputManager {
+    func addStudentGradeInfo() -> Result<(name: String, subject: String, grade: String), InputError> {
+        guard let gradeInfo = readLine() else {
+            return .failure(.invalidUserInput)
+        }
+        
+        let validStudentInfoLength = 3
+        let nameIndex = 0
+        let subjectIndex = 1
+        let gradeIndex = 2
+        
+        let studentInfo = gradeInfo.components(separatedBy: " ")
+        guard studentInfo.count == validStudentInfoLength else {
+            return .failure(.invalidStudentGradeInfoInput)
+        }
+                
+        return .success((name: studentInfo[nameIndex], subject: studentInfo[subjectIndex], grade: studentInfo[gradeIndex]))
+    }
+    
+    func deleteStudentGradeInfo() -> Result<(name: String, subject: String), InputError> {
+        guard let gradeInfo = readLine() else {
+            return .failure(.invalidUserInput)
+        }
+        
+        let validStudentInfoLength = 2
+        let nameIndex = 0
+        let subjectIndex = 1
+        
+        let studentInfo = gradeInfo.components(separatedBy: " ")
+        guard studentInfo.count == validStudentInfoLength else {
+            return .failure(.invalidStudentGradeInfoInput)
+        }
+        
+        return .success((name: studentInfo[nameIndex], studentInfo[subjectIndex]))
+    }
+    
     func choiceMenu() -> Result<Menu, InputError> { // 1,2,3,4,5, X, 얘네를 제외하고 다른 문자가 들어가면
         guard let choice = readLine(), let menu = Menu(rawValue: choice) else {
             return .failure(.invalidUserInput)
@@ -45,12 +83,24 @@ struct InputManager {
 }
 
 protocol StudentManager {
-    mutating func add(name: String)
-    mutating func delete(name: String)
+    mutating func addStudent(name: String)
+    mutating func deleteStudent(name: String)
+    mutating func addOrChangeGrade(name: String, subject: String, grade: String)
+    mutating func deleteGrade(name: String, subject: String)
 }
 
-struct Student: Hashable {
-    var name: String
+class Student: Hashable {
+    private var name: String
+    private var courseManager: CourseManager
+    
+    init(name: String) {
+        self.name = name
+        self.courseManager = CourseManager()
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
     
     static func == (lhs: Student, rhs: Student) -> Bool {
         return lhs.name == rhs.name
@@ -59,6 +109,94 @@ struct Student: Hashable {
     static func == (lhs: Student, rhs: String) -> Bool {
         return lhs.name == rhs
     }
+    
+    func addOrChangeGrade(subject: String, grade: String) {
+        courseManager.add(name: name, subject: subject, grade: grade)
+    }
+    
+    func deleteGrade(subject: String) {
+        courseManager.delete(name: name, subject: subject)
+    }
+}
+
+struct CourseManager {
+    private enum InfoMessage: String {
+        case invalidGrade = "잘못된 성적입니다."
+        case notExistSubject = "성적이 존재하지 않는 과목입니다."
+        case addOrChangeGrade
+        case deleteGrade
+        
+        func printing(name: String? = nil, subject: String? = nil, grade: String? = nil) {
+            switch self {
+            case .invalidGrade, .notExistSubject:
+                print(self.rawValue)
+            case .addOrChangeGrade:
+                guard let name = name, let subject = subject, let grade = grade else {
+                    return
+                }
+                print("\(name) 학생의 \(subject) 과목이 \(grade)로 추가(변경) 되었습니다.")
+            case .deleteGrade:
+                guard let name = name, let subject = subject else {
+                    return
+                }
+                print("\(name) 학생의 \(subject) 과목의 성적이 삭제되었습니다.")
+            }
+        }
+    }
+    
+    private enum Grade: String {
+        case Aplus = "A+"
+        case Azero = "A0"
+        case Bplus = "B+"
+        case Bzero = "B0"
+        case Cplus = "C+"
+        case Czero = "C0"
+        case Dplus = "D+"
+        case Dzero = "D0"
+        case F = "F"
+        
+        var score: Double {
+            switch self {
+            case .Aplus:
+                return 4.5
+            case .Azero:
+                return 4.0
+            case .Bplus:
+                return 3.5
+            case .Bzero:
+                return 3.0
+            case .Cplus:
+                return 2.5
+            case .Czero:
+                return 2.0
+            case .Dplus:
+                return 1.5
+            case .Dzero:
+                return 1.0
+            case .F:
+                return 0.0
+            }
+        }
+    }
+    private var list: [String: Grade] = [:]
+    
+    mutating func add(name: String, subject: String, grade: String) {
+        guard let gradeLevel = Grade(rawValue: grade) else {
+            InfoMessage.invalidGrade.printing()
+            return
+        }
+        list[subject] = gradeLevel
+        InfoMessage.addOrChangeGrade.printing(name: name, subject: subject, grade: grade)
+    }
+    
+    mutating func delete(name: String, subject: String) {
+        guard let _ = list[subject] else {
+            InfoMessage.notExistSubject.printing()
+            return
+        }
+        list[subject] = nil
+        InfoMessage.deleteGrade.printing(name: name, subject: subject)
+    }
 }
 
 struct DefaultStudentManager: StudentManager {
@@ -66,11 +204,11 @@ struct DefaultStudentManager: StudentManager {
         case addSuccess = " 학생을 추가했습니다."
         case addFailure = "입력이 잘못되었습니다. 다시 확인해주세요."
         case deleteSuccess = "학생을 삭제하였습니다."
-        case deleteFailure = " 학생을 찾지 못했습니다."
+        case canNotFindStudent = " 학생을 찾지 못했습니다."
         
         func printing(name: String) {
             switch self {
-            case .addSuccess, .deleteFailure:
+            case .addSuccess, .canNotFindStudent:
                 print(name, terminator: "")
                 print(self.rawValue)
             default:
@@ -85,7 +223,7 @@ struct DefaultStudentManager: StudentManager {
         return self.list.count
     }
     
-    func isDuplicate(name: String) -> Bool {
+    func isExist(name: String) -> Bool {
         return list.contains(Student(name: name))
     }
     
@@ -98,8 +236,8 @@ struct DefaultStudentManager: StudentManager {
         return nil
     }
     
-    mutating func add(name: String) {
-        if isDuplicate(name: name) {
+    mutating func addStudent(name: String) {
+        if isExist(name: name) {
             InfoMessage.addFailure.printing(name: name)
             return
         }
@@ -107,13 +245,39 @@ struct DefaultStudentManager: StudentManager {
         InfoMessage.addSuccess.printing(name: name)
     }
     
-    mutating func delete(name: String) {
+    mutating func deleteStudent(name: String) {
         guard let deleteTargetStudent = deleteTarget(name: name) else {
-            InfoMessage.deleteFailure.printing(name: name)
+            InfoMessage.canNotFindStudent.printing(name: name)
             return
         }
         list.remove(deleteTargetStudent)
         InfoMessage.deleteSuccess.printing(name: name)
+    }
+    
+    mutating func addOrChangeGrade(name: String, subject: String, grade: String) {
+        guard isExist(name: name) else {
+            InfoMessage.canNotFindStudent.printing(name: name)
+            return
+        }
+        for student in list {
+            if student == name {
+                student.addOrChangeGrade(subject: subject, grade: grade)
+                return
+            }
+        }
+    }
+    
+    mutating func deleteGrade(name: String, subject: String) {
+        guard isExist(name: name) else {
+            InfoMessage.canNotFindStudent.printing(name: name)
+            return
+        }
+        for student in list {
+            if student == name {
+                student.deleteGrade(subject: subject)
+                return
+            }
+        }
     }
 }
 
@@ -123,8 +287,10 @@ struct GradeManager {
     
     private enum InfoMessage: String {
         case guide = "원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료"
-        case add = "추가할 학생의 이름을 입력해주세요"
-        case delete = "삭제할 학생의 이름을 입력해주세요"
+        case requireStudentNameToAdd = "추가할 학생의 이름을 입력해주세요"
+        case requireStudentNameToDelete = "삭제할 학생의 이름을 입력해주세요"
+        case requireStudentGradeToAddOrChange = "성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A0, F등)을 띄어쓰기로 구분하여 차례로 작성해주세요.\n입력예) Mickey Swift A+"
+        case requireStudentGradeToDelete = "성적을 삭제할 학생의 이름, 과목 이름을 띄어쓰기로 구분하여 차례로 작성해주세요."
         case exit = "프로그램을 종료합니다..."
         
         func printing() {
@@ -144,25 +310,51 @@ struct GradeManager {
     }
     
     mutating func addStudent() {
-        InfoMessage.add.printing()
+        InfoMessage.requireStudentNameToAdd.printing()
         let studentNameResult = inputManager.getStudentName()
         switch studentNameResult {
         case .success(let studentName):
-            studentManager.add(name: studentName)
+            studentManager.addStudent(name: studentName)
         case .failure(let error):
             print(error.localizedDescription)
         }
     }
     
     mutating func deleteStudent() {
-        InfoMessage.delete.printing()
+        InfoMessage.requireStudentNameToDelete.printing()
         let studentNameResult = inputManager.getStudentName()
         switch studentNameResult {
         case .success(let studentName):
-            studentManager.delete(name: studentName)
+            studentManager.deleteStudent(name: studentName)
         case . failure(let error):
             print(error.localizedDescription)
         }
+    }
+    
+    mutating func addOrChangeGrade() {
+        InfoMessage.requireStudentGradeToAddOrChange.printing()
+        let studentGradeInfo = inputManager.addStudentGradeInfo()
+        switch studentGradeInfo {
+        case .success(let gradeInfo):
+            studentManager.addOrChangeGrade(name: gradeInfo.name,
+                                            subject: gradeInfo.subject,
+                                            grade: gradeInfo.grade)
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+    
+    mutating func deleteGrade() {
+        InfoMessage.requireStudentGradeToDelete.printing()
+        let studentGradeDeleteInfo = inputManager.deleteStudentGradeInfo()
+        switch studentGradeDeleteInfo {
+        case .success(let gradeInfo):
+            studentManager.deleteGrade(name: gradeInfo.name,
+                                       subject: gradeInfo.subject)
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+        
     }
     
     mutating private func operate(menu: Menu) {
@@ -171,6 +363,10 @@ struct GradeManager {
             addStudent()
         case .deleteStudent:
             deleteStudent()
+        case .addOrChangeGrade:
+            addOrChangeGrade()
+        case .deleteGrade:
+            deleteGrade()
         default:
             break
         }
