c40f53d07b959066224dff6b0f648619fdd38376
volgaHwang <wltkak2@gmail.com>
2022-12-29 14:23:07 +0900

    test: StudentManager에서 학생 추가 / 삭제에 관한 테스트 케이스를 반영합니다.
    - StudentManager에서 학생 추가 / 삭제에 관련된 테스트케이스를 반영했습니다.
        - 학생 추가 시, 중복이 될 경우와, 중복이 되지 않는 경우에 대한 테스트케이스를 생성했습니다.
        - 학생 삭제 시, 중복인 이름을 삭제 할 경우에 대해서 테스트케이스를 생성했습니다.

--- UnitTestGradeManager/UnitTestSutdentManager.swift
+++ UnitTestGradeManager/UnitTestSutdentManager.swift
@@ -9,8 +9,86 @@ import XCTest
 
 final class UnitTestSutdentManager: XCTestCase {
     
-    let sut = DefaultStudentManager()
+    var sut = DefaultStudentManager()
     
+    func randomStudent() -> String {
+        let student1 = "summer.love"
+        let student2 = "volga.poco"
+        let student3 = "forest.jay"
+        let student4 = "john.lim"
+        let student5 = "yagom"
+        let student6 = "kio"
+        let student7 = "수박"
+        let student8 = "라자냐"
+        let students = [student1, student2, student3, student4, student5, student6, student7, student8]
+        
+        return students.shuffled()[0]
+    }
     
+    func test_학생을_추가했을_경우_학생들의_수가_1_증가한다() {
+        let studentCountBeforeAdd = sut.numberOfStudent
+        sut.add(name: randomStudent())
+        let studentCountAfterAdd = sut.numberOfStudent
+        XCTAssertEqual(studentCountBeforeAdd + 1, studentCountAfterAdd)
+    }
     
+    func test_있는_학생을_추가했을_경우_학생들의_수가_증가_하지_않는다() {
+        let studentName = randomStudent()
+        
+        sut.add(name: studentName)
+        let studentCountBeforeAdd = sut.numberOfStudent
+        
+        sut.add(name: studentName)
+        let studentCountAfterAdd = sut.numberOfStudent
+        XCTAssertEqual(studentCountBeforeAdd, studentCountAfterAdd)
+    }
+    
+    func test_학생을_추가했을_경우_입력한_이름이_올바르게_들어간다() {
+        let studentName = randomStudent()
+        
+        let isStudentExistBeforeAdd = sut.isDuplicate(name: studentName)
+        XCTAssertFalse(isStudentExistBeforeAdd)
+        
+        sut.add(name: studentName)
+        
+        let isStudentExistAfterAdd = sut.isDuplicate(name: studentName)
+        XCTAssertTrue(isStudentExistAfterAdd)
+    }
+    
+    func test_학생을_삭제_했을_경우_학생들의_수가_1_감소한다() {
+        let studentName = randomStudent()
+        sut.add(name: studentName)
+        
+        let studentCountBeforeAdd = sut.numberOfStudent
+        sut.delete(name: studentName)
+        
+        let studentCountAfterAdd = sut.numberOfStudent
+        XCTAssertEqual(studentCountBeforeAdd - 1, studentCountAfterAdd)
+    }
+    
+    func test_없는_학생을_삭제_했을_경우_학생들의_수가_1_감소하지_않는다() {
+        let studentName = randomStudent()
+        sut.add(name: studentName)
+        
+        sut.delete(name: studentName)
+        let studentCountBeforeAdd = sut.numberOfStudent
+        
+        sut.delete(name: studentName)
+        let studentCountAfterAdd = sut.numberOfStudent
+        
+        XCTAssertEqual(studentCountBeforeAdd, studentCountAfterAdd)
+    }
+    
+    func test_학생을_삭제_할_경우_입력한_이름이_올바르게_삭제된다() {
+        let studentName = randomStudent()        
+        sut.add(name: studentName)
+        
+        let isStudentExistBeforeAdd = sut.isDuplicate(name: studentName)
+        XCTAssertTrue(isStudentExistBeforeAdd)
+        
+        sut.delete(name: studentName)
+        
+        let isStudentExistAfterAdd = sut.isDuplicate(name: studentName)
+        XCTAssertFalse(isStudentExistAfterAdd)
+    }
 }
