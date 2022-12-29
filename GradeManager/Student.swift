207289aa877345ea664ad71111a5742f39fcfaa1
john.lim <limjae96@gmail.com>
2022-12-29 15:42:47 +0900

    feat: 학생의 성적 업데이트 기능 추가
    - 내부 property 변경으로 인한 mutating method 추가

--- GradeManager/Student.swift
+++ GradeManager/Student.swift
@@ -10,4 +10,12 @@ import Foundation
 struct Student {
     let name: String
     private(set) var scores: [String: Grade] = [:]
+    
+    mutating func addScore(subject: String, grade: Grade) {
+        scores[subject] = grade
+    }
+    
+    mutating func deleteScore(subject: String) {
+        
+    }
 }
