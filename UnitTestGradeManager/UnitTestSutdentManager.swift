66d3c90bf8e78c5678458e22e8654f5e606c5748
volgaHwang <wltkak2@gmail.com>
2022-12-29 14:03:52 +0900

    test: StudentManager 동작을 위한 테스트 파일 추가 반영

---
+++ UnitTestGradeManager/UnitTestSutdentManager.swift
@@ -0,0 +1,16 @@
+//
+//  UnitTestSutdentManager.swift
+//  UnitTestGradeManager
+//
+//  Created by kakao on 2022/12/29.
+//
+
+import XCTest
+
+final class UnitTestSutdentManager: XCTestCase {
+    
+    let sut = DefaultStudentManager()
+    
+    
+    
+}
